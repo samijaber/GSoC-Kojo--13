787ef5f404d45473f933829a9f6c61c626a7a399
Sami Jaber <jabersami@gmail.com>
2013-05-06 00:10:36 -0400

    Connected VM

--- ProgramA.scala
+++ ProgramA.scala
@@ -2,9 +2,8 @@ import scala.collection.JavaConversions.asScalaBuffer
 import scala.collection.JavaConversions.asScalaIterator
  
 import com.sun.jdi.connect.Connector
-import com.sun.jdi.event.BreakpointEvent
-import com.sun.jdi.request.BreakpointRequest
-import com.sun.jdi.request.EventRequest
+import com.sun.jdi.event.MethodExitEvent
+import com.sun.jdi.request._
 import com.sun.jdi.Bootstrap
 import com.sun.jdi.StringReference
  
@@ -23,5 +22,22 @@ object ProgramA extends App {
   val vm = socketConnector.attach(paramsMap)
   println("Attached to process '" + vm.name + "'")
 
-  println(vm.canGetMethodReturnValues())
+//  vm.setDebugTrace(VirtualMachine.TRACE_ALL)
+  //val evtthrd : EventThread = new EventThread(vm, )
+
+  
+  val evtReqMgr = vm.eventRequestManager
+  val mthdExVal = evtReqMgr.createMethodExitRequest()
+  mthdExVal.enable()
+
+  val evtQueue = vm.eventQueue
+
+  val evtSet = evtQueue.remove()
+  for (evt <- evtSet.eventIterator) {
+      evt match {
+        case method_evt: MethodExitEvent => println("The value is " + method_evt.returnValue())
+        case all => println("whatever " + all.request)
+        }
+    }
+
 }
--- Wrapper.scala
+++ Wrapper.scala
@@ -1,6 +1,5 @@
 object Wrapper {
   def main(args: Array[String]) {
-  	println(factorial(10))
   }
   def factorial(n: Int): Int = if (n == 0) 1 else n * factorial(n-1)
   factorial(5)
